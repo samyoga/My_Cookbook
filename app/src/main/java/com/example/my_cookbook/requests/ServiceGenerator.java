1353d4200d27add04ed9581c40133fe5acf5ab2e
samyoga <bhattaraisamyoga@gmail.com>
2019-09-16 15:59:16 +0545

    retrofit initialized

---
+++ app/src/main/java/com/example/my_cookbook/requests/ServiceGenerator.java
@@ -0,0 +1,17 @@
+package com.example.my_cookbook.requests;
+
+import com.example.my_cookbook.util.Constants;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceGenerator {
+
+    private static Retrofit.Builder retrofitBuilder =
+            new Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = retrofitBuilder.build();
+
+}
