9b8191d6404e61335e72526cb46f4a766b4d1d83
samyoga <bhattaraisamyoga@gmail.com>
2019-09-16 13:44:50 +0545

    Base activity created

--- app/src/main/java/com/example/my_cookbook/BaseActivity.java
+++ app/src/main/java/com/example/my_cookbook/BaseActivity.java
@@ -1,16 +1,29 @@
 package com.example.my_cookbook;
 
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
+    private ProgressBar mprogressBar;
+
     @Override
     public void setContentView(int layoutResID) {
         ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base, null);
         FrameLayout frameLayout = constraintLayout.findViewById(R.id.activity_content);
+        mprogressBar = constraintLayout.findViewById(R.id.progress_bar);
+
+        getLayoutInflater().inflate(layoutResID, frameLayout, true);
+
         super.setContentView(layoutResID);
+    }
+
+    public void showProgressBar(boolean visibility){
+        mprogressBar.setVisibility(visibility ? View.VISIBLE : View.INVISIBLE);
+
     }
 }
