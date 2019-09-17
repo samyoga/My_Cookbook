9bcb9a6085e8feebd5c6cd58d5fc1e5160422033
samyoga <bhattaraisamyoga@gmail.com>
2019-09-17 10:40:21 +0545

    interface for fetching recipe on request created using get method

--- app/src/main/java/com/example/my_cookbook/requests/RecipeAPI.java
+++ app/src/main/java/com/example/my_cookbook/requests/RecipeAPI.java
@@ -1,5 +1,26 @@
 package com.example.my_cookbook.requests;
 
+import com.example.my_cookbook.requests.responses.RecipeResponse;
+import com.example.my_cookbook.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeAPI {
 
+    //search
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    //get recipe request
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(
+            @Query("key") String key,
+            @Query("rId") String recipe_id
+    );
 }
