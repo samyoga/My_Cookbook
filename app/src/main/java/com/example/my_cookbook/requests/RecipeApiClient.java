6989a83fc02d229d24debca1f067c13888bb6500
samyoga <bhattaraisamyoga@gmail.com>
2019-09-17 15:28:02 +0545

    Executed a runnable using executor

--- app/src/main/java/com/example/my_cookbook/requests/RecipeApiClient.java
+++ app/src/main/java/com/example/my_cookbook/requests/RecipeApiClient.java
@@ -1,21 +1,33 @@
 package com.example.my_cookbook.requests;
 
+import android.app.DownloadManager;
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.example.my_cookbook.AppExecutors;
 import com.example.my_cookbook.models.Recipe;
+import com.example.my_cookbook.requests.responses.RecipeSearchResponse;
+import com.example.my_cookbook.util.Constants;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 import static com.example.my_cookbook.util.Constants.NETWORK_TIMEOUT;
 
 public class RecipeApiClient {
 
+    private static final String TAG = "RecipeApiClient";
     private static RecipeApiClient instance;
     private MutableLiveData<List<Recipe>> mRecipes;
+    private RetrieveRecipesRunnable mRetrieveRecipesRunnable;
 
     public static RecipeApiClient getInstance(){
         if (instance == null){
@@ -32,14 +44,12 @@ public class RecipeApiClient {
         return mRecipes;
     }
 
-    public void searchRecipesApi(){
-        final Future handler = AppExecutors.getInstance().getmNetworkIO().submit(new Runnable() {
-            @Override
-            public void run() {
-                //retrieve data from REST API
-//                mRecipes.postValue();
-            }
-        });
+    public void searchRecipesApi(String query, int pageNumber){
+        if (mRetrieveRecipesRunnable != null){
+            mRetrieveRecipesRunnable = null;
+        }
+        mRetrieveRecipesRunnable = new RetrieveRecipesRunnable(query, pageNumber);
+        final Future handler = AppExecutors.getInstance().getmNetworkIO().submit(mRetrieveRecipesRunnable);
 
         AppExecutors.getInstance().getmNetworkIO().schedule(new Runnable() {
             @Override
@@ -49,5 +59,59 @@ public class RecipeApiClient {
                 handler.cancel(true);
             }
         }, NETWORK_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
+
+    private class RetrieveRecipesRunnable implements Runnable{
+
+        private String query;
+        private int pageNumber;
+        boolean cancelRequest;
+
+        public RetrieveRecipesRunnable(String query, int pageNumber) {
+            this.query = query;
+            this.pageNumber = pageNumber;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Response response = getRecipes(query, pageNumber).execute();
+                if (cancelRequest) {
+                    return;
+                }
+                if (response.code() == 200){
+                   List<Recipe> list = new ArrayList<>(((RecipeSearchResponse)response.body()).getRecipes());
+                   if (pageNumber == 1){
+                       mRecipes.postValue(list);
+                   }else {
+                       List<Recipe> currentRecipes = mRecipes.getValue();
+                       currentRecipes.addAll(list);
+                       mRecipes.postValue(currentRecipes);
+                   }
+
+                }else{
+                    String error = response.errorBody().string();
+                    Log.e(TAG, "error: " + error);
+                    mRecipes.postValue(null);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                mRecipes.postValue(null);
+            }
+        }
+
+        private Call<RecipeSearchResponse> getRecipes(String query, int pageNumber){
+            return ServiceGenerator.getRecipeAPI().searchRecipe(
+                    Constants.BASE_URL,
+                    query,
+                    String.valueOf(pageNumber)
+            );
+        }
+
+        private void cancelRequest(){
+            Log.d(TAG, "cancelRequest: canceling the search request");
+            cancelRequest = true;
+        }
     }
 }
