9134dbdbf745037e1ece63ee9136f0317ef3a314
samyoga <bhattaraisamyoga@gmail.com>
2019-09-19 11:18:13 +0545

    Created activity for Recipe ingredients display

---
+++ app/src/main/java/com/example/my_cookbook/RecipeActivity.java
@@ -0,0 +1,38 @@
+package com.example.my_cookbook;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import com.example.my_cookbook.models.Recipe;
+
+public class RecipeActivity extends BaseActivity {
+
+    private AppCompatImageView mRecipeImage;
+    private TextView mRecipeTitle, mRecipeRank;
+    private LinearLayout mRecipeIngredientsContainer;
+    private ScrollView mScrollView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+        mRecipeImage = findViewById(R.id.recipe_image);
+        mRecipeTitle = findViewById(R.id.recipe_title);
+        mRecipeRank = findViewById(R.id.recipe_social_score);
+        mRecipeIngredientsContainer = findViewById(R.id.ingredients_container);
+        mScrollView = findViewById(R.id.parent);
+
+        getIncomingIntent();
+    }
+
+    private void getIncomingIntent(){
+        if (getIntent().hasExtra("recipe")){
+            Recipe recipe = getIntent().getParcelableExtra("recipe");
+        }
+    }
+}
